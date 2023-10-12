90a38155ec55af5067b1db0aab942fd1fbf73192
Ho-Feng <Ho-Feng.Wong@whitevision.nl>
2023-10-12 12:30:00 +0200

    update: change to full screen

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className={`w-full h-screen overflow-y-auto flex justify-center ${inter.className}`}>
-      <section className="w-full flex flex-col gap-4 max-w-[800px] relative bg-gray-100 border-black overflow-y-auto">
+      <section className="w-full flex flex-col gap-4 relative bg-gray-100 border-black overflow-y-auto">
         <Component {...pageProps} />
       </section>
     </div>
