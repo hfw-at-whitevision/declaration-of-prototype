9595a4739e1e1892265744ab564e961de21772b2
Ho-Feng <Ho-Feng.Wong@whitevision.nl>
2023-10-30 14:48:36 +0100

    UPDATE: deeplink demo in dashboard

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,18 +5,25 @@ import PlusMenu from "@/components/declarations/PlusMenu";
 import WaitingForApprovalList from "@/components/dashboard/WaitingForApprovalList";
 import RejectionsList from "@/components/dashboard/RejectionsList";
 import ActionsGrid from "@/components/dashboard/ActionsGrid";
+import {useRouter} from "next/router";
 
 export default function HomePage() {
     const isApprover = process.env.NEXT_PUBLIC_IS_APPROVER === 'true';
+    const router = useRouter();
+    const query = router.query;
+    const name = query?.name;
+    const email = query?.email;
+    const company = query?.company;
+
     return <>
         <DashboardHeader/>
 
         <Content className="">
 
             <section className="py-16 flex flex-col text-center">
-                <h1 className="font-extrabold text-4xl">Welkom Heinz!</h1>
-                <small className="mt-4 opacity-50">heinz@hotmail.com</small>
-                <small className="opacity-50">12345 Bouwbedrijf de Steen</small>
+                <h1 className="font-extrabold text-4xl">Welkom {name ?? 'onbekende'}!</h1>
+                <small className="mt-4 opacity-50">{email ?? 'anoniempje'}</small>
+                <small className="opacity-50">{company ?? 'anoniem bedrijf'}</small>
             </section>
             {isApprover
                 ? <WaitingForApprovalList/>
--- src/components/dashboard/RejectionsList.tsx
+++ src/components/dashboard/RejectionsList.tsx
@@ -4,11 +4,11 @@ import {BsArrowRight} from "react-icons/bs";
 import {useRouter} from "next/router";
 
 export default function RejectionsList({className = ''}) {
-    const router = useRouter();
     return <section className={`flex flex-col gap-2 ${className}`}>
         <h2 className="font-extrabold text-2xl">
             Afgekeurd
         </h2>
+
         <section className="grid gap-2">
             <Card borderLeft={true} borderColor="bg-amber-400">
                     <span className="font-bold">
