18bb96261aa51bd6d884b61f393f1124ef2503a1
Ho-Feng <Ho-Feng.Wong@whitevision.nl>
2023-10-30 11:53:12 +0100

    UPDATE: deeplinks assetlinks.json

--- src/hooks/useApp.ts
+++ src/hooks/useApp.ts
@@ -3,17 +3,17 @@ import {useEffect} from "react";
 import {useRouter} from "next/router";
 
 const registerListeners = () => {
-    App.addListener('appStateChange', ({isActive}) => {
-        console.log('useApp: App state changed. Is active?', isActive);
-    });
+    // App.addListener('appStateChange', ({isActive}) => {
+    //     console.log('useApp: App state changed. Is active?', isActive);
+    // });
 
     App.addListener('appUrlOpen', data => {
         console.log('useApp: App opened with URL:', data);
     });
 
-    App.addListener('appRestoredResult', data => {
-        console.log('useApp: Restored state:', data);
-    });
+    // App.addListener('appRestoredResult', data => {
+    //     console.log('useApp: Restored state:', data);
+    // });
 }
 
 const checkAppLaunchUrl = async () => {
